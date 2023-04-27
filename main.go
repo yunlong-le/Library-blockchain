cf3f3b44764d51e4c5fe81d350ec700f73a73b76
wangtong <realwangtong@163.com>
2023-04-27 10:59:21 +0800

    1.修改main方法兼容external模式

--- main.go
+++ main.go
@@ -2,14 +2,47 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"os"
 
 	"github.com/hyperledger/fabric-chaincode-go/shim"
 	"github.com/yunlong-le/library/chaincode-2"
 )
 
+type serverConfig struct {
+	CCID    string
+	Address string
+}
+
 func main() {
-	err := shim.Start(new(chaincode.SmartContract))
-	if err != nil {
-		fmt.Printf("Error starting Simple chaincode: %s", err)
+	mode := os.Getenv("MODE")
+	if mode == "chaincode" {
+		// 链码模式启动逻辑
+		err := shim.Start(new(chaincode.SmartContract))
+		if err != nil {
+			fmt.Printf("Error starting Simple chaincode: %s", err)
+		}
+	} else if mode == "external" {
+		// 外部模式启动逻辑
+		config := serverConfig{
+			CCID:    os.Getenv("CHAINCODE_ID"),
+			Address: os.Getenv("CHAINCODE_SERVER_ADDRESS"),
+		}
+		//chaincode, err := contractapi.NewChaincode(&SmartContract{})
+
+		cc := new(chaincode.SmartContract)
+		server := &shim.ChaincodeServer{
+			CCID:    config.CCID,
+			Address: config.Address,
+			CC:      cc,
+			TLSProps: shim.TLSProperties{
+				Disabled: true,
+			},
+		}
+		if err := server.Start(); err != nil {
+			log.Panicf("Error starting asset-transfer-basic chaincode: %s", err.Error())
+		}
+	} else {
+		log.Panic("Invalid mode specified")
 	}
 }
